f24bae3dabc93d30f47058a5f9749332fb137641
Can Emir Bora <emir27_bora@hotmail.com>
2026-03-05 22:06:05 +0300

    Update comment in predict.py to remove projection years
    Removed the projection years from the comment.

--- predict.py
+++ predict.py
@@ -1,1 +1,1 @@
-# İklim tahmin modeli: sayısallaştırılmış nem ve basınç verisini Prophet ile analiz eder, 2025-2030 projeksiyonu üretir
+# İklim tahmin modeli: sayısallaştırılmış nem ve basınç verisini Prophet ile analiz eder

8259fdc2b7e5744846ca69858757f03b510d8641
Can Emir Bora <emir27_bora@hotmail.com>
2026-03-05 20:47:33 +0300

    Added project core files

---
+++ digitize.py
@@ -0,0 +1,1 @@
+# CV pipeline: TIF grafik kağıtlarından ızgara kalibrasyonu ve eğri izolasyonu ile sayısal veri çıkarır, CSV olarak kaydeder
---
+++ predict.py
@@ -0,0 +1,1 @@
+# İklim tahmin modeli: sayısallaştırılmış nem ve basınç verisini Prophet ile analiz eder, 2025-2030 projeksiyonu üretir
